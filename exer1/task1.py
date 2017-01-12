c3af78084d64cf5105d10d4aa353b6fe302586e0
alitakin <saboktakinhayati@gmail.com>
2017-01-12 12:01:13 +0200

    firstExercise

---
+++ exer1/task1.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 12 10:59:56 2017
+
+@author: aliTakin
+"""
+
+import numpy as np
+
+loaded=np.loadtxt(r"C:\Users\aliTakin\Desktop\4.92\sgn_41007\locationData\locationData.csv")
+size=np.shape(loaded)
